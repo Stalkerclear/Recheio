96775dbe4903435962d3ca8552196b4381703f78
Stalkerclear <stalkernois64@gmail.com>
2025-04-19 16:35:36 -0300

    Update track.js

--- api/track.js
+++ api/track.js
@@ -1,22 +1,29 @@
-let data = global.data || [];
-global.data = data;
+const axios = require('axios');
 
-export default async function handler(req, res) {
+let data = [];
+
+module.exports = async (req, res) => {
   const { dest } = req.query;
-  if (!dest) return res.status(400).send('Missing destination');
+  if (!dest) return res.status(400).send('Missing dest');
 
   const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
-  let geo = {};
 
+  let geo = {};
   try {
-    const response = await fetch(`http://ip-api.com/json/${ip}`);
-    geo = await response.json();
-  } catch (e) {
-    geo = { city: 'Unknown', country: 'Unknown', isp: 'Unknown' };
+    const response = await axios.get(`http://ip-api.com/json/${ip}`);
+    geo = {
+      city: response.data.city || '-',
+      country: response.data.country || '-',
+      isp: response.data.isp || '-'
+    };
+  } catch (err) {
+    console.error('Geo error:', err.message);
   }
 
   data.push({ ip, ...geo, timestamp: new Date().toISOString() });
 
-  res.writeHead(302, { Location: decodeURIComponent(dest) });
+  res.writeHead(302, { Location: dest });
   res.end();
-}
+};
+
+module.exports.data = () => data;
